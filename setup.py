2ae8303a8799734286044ca8c46d9f767e6b8c87
np <np@exemple.com>
2017-07-09 22:12:41 +0200

    Add compile mo files when setup process

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import subprocess
+
 from setuptools import setup, find_packages
 from cms_bootstrap4 import __version__
 try:
@@ -9,6 +11,25 @@ except ImportError:
     def convert(filename, fmt):
         with open(filename) as fd:
             return fd.read()
+
+def create_mo_files():
+    data_files = []
+    localedir = 'cms_bootstrap4/locale'
+    po_dirs = [localedir + '/' + l + '/LC_MESSAGES/'
+               for l in next(os.walk(localedir))[1]]
+    for d in po_dirs:
+        mo_files = []
+        po_files = [f
+                    for f in next(os.walk(d))[2]
+                    if os.path.splitext(f)[1] == '.po']
+        for po_file in po_files:
+            filename, extension = os.path.splitext(po_file)
+            mo_file = filename + '.mo'
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(d + po_file, d + mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo_files.append(d + mo_file)
+        data_files.append((d, mo_files))
+    return data_files
 
 
 CLASSIFIERS = [
@@ -41,5 +62,6 @@ setup(
     classifiers=CLASSIFIERS,
     long_description=convert('README.md', 'rst'),
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    data_files=create_mo_files(),
 )
