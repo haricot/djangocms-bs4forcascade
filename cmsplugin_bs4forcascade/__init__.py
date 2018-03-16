1070893d102c1168244ac1ca4df37ca48c12578a
Nicolas PASCAL <np.pascal@gmail.com>
2018-03-16 13:31:14 +0100

    Bump to version 0.15.4v2bs4

--- cmsplugin_bs4forcascade/__init__.py
+++ cmsplugin_bs4forcascade/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = "0.15.3v2bs4"
+__version__ = "0.15.4v2bs4"
 
 #default_app_config = 'cmsplugin_cascade.apps.CascadeConfig'
