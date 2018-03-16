ad5914025b1e32a8b79a5921344b9d0a1b3a58c3
Nicolas PASCAL <np.pascal@gmail.com>
2018-03-16 12:51:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     license='LICENSE-MIT',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
-    long_description=convert('README.md', 'rst'),
+    #long_description=convert('README.md', 'rst'),
     include_package_data=True,
     zip_safe=False,
 )
